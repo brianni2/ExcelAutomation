c6a96991966fb65566a7489c417084eaba8bb76a
brianni2 <bni2@binghamton.edu>
2023-06-09 14:06:59 -0400

    tested

--- excelDatabase.py
+++ excelDatabase.py
@@ -6,12 +6,12 @@ import numpy as np
 from excelFormatter import ExcelFormatter
 
 class ExcelDatabase:
-    def __init__(self):
-        self.driver = input('Enter driver name: ')
-        self.server = input('Enter server name: ')
-        self.database = input('Enter database name: ')
-        self.UID = input('Enter username: ')
-        self.PWD = input('Enter password: ')
+    def __init__(self, driver, server, database, UID="", PWD=""):
+        self.driver = driver
+        self.server = server
+        self.database = database
+        self.UID = UID
+        self.PWD = PWD
 
     def connect(self):
         try:
@@ -27,31 +27,32 @@ class ExcelDatabase:
     
     def get_data(self, query):
         try:
-            self.data = pd.read_sql_query(query, self.db)
+            self.df = pd.read_sql_query(query, self.db)
             print('Data retrieved')
             return True
         except pd.errors.DatabaseError as e:
             print(e)
             return False
         
-    def write_data(self, file_name, sheet_name):
+    def write_data(self, file_name, working_sheet_name):
         try:
             with pd.ExcelWriter(file_name, engine='openpyxl', mode='w') as writer:
-                self.data.to_excel(writer, sheet_name=sheet_name, index=False)
+                self.df.to_excel(writer, sheet_name=working_sheet_name, index=False)
             print('Data written')
             return True
         except Exception as e:
             print(e)
             return False
         
-    def generic_format(self, file_name, sheet_name, save_name=None):
+    def generic_format(self, file_name, working_sheet_name, save_name=None):
         lastColumn = openpyxl.utils.get_column_letter(self.df.shape[1])
         self.get_column_length()
         try:
             wb = openpyxl.load_workbook(file_name)
-            ws = self.wb[sheet_name]
+            ws = wb[working_sheet_name]
             # Set column widths to fit
-            ExcelFormatter.setColumnWidth(ws, (1, lastColumn), self.column_length[i])
+            for i in range(len(self.column_length)):
+                ws.column_dimensions[openpyxl.utils.get_column_letter(i+1)].width = self.column_length[i]
             # Generic Column Formatting
             ExcelFormatter.formatAxis(ws, (1, lastColumn), 1, Font(), PatternFill(), 
                                         Border(left=Side(border_style="thin", color="00000000"), right=Side(border_style="thin", color="00000000"), 
